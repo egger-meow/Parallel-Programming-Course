54c9f01e3f07894fe88895657e704d7429495341
jjmow <dagantegan.ms09@nctu.edu.tw>
2024-10-29 04:00:52 +0800

    recover

--- HW3/part2/page_rank/page_rank.cpp
+++ HW3/part2/page_rank/page_rank.cpp
@@ -1,73 +1,99 @@
 #include "page_rank.h"
+
 #include <stdlib.h>
 #include <cmath>
 #include <omp.h>
 #include <utility>
+
 #include "../common/CycleTimer.h"
 #include "../common/graph.h"
 
+// pageRank --
+//
+// g:           graph to process (see common/graph.h)
+// solution:    array of per-vertex vertex scores (length of array is num_nodes(g))
+// damping:     page-rank algorithm's damping parameter
+// convergence: page-rank algorithm's convergence threshold
+//
 void pageRank(Graph g, double *solution, double damping, double convergence)
 {
+
+  // initialize vertex weights to uniform probability. Double
+  // precision scores are used to avoid underflow for large graphs
+
     int numNodes = num_nodes(g);
     double equal_prob = 1.0 / numNodes;
-    
-    // Initialize solution array
-    #pragma omp parallel for
-    for (int i = 0; i < numNodes; ++i) {
+    for (int i = 0; i < numNodes; ++i)
+    {
         solution[i] = equal_prob;
     }
 
     double *scoreOld = (double *) malloc(numNodes * sizeof(double));
+    // double *scoreNew = (double *) malloc(numNodes * sizeof(double));
+
     bool converge = false;
-    
-    while (!converge) {
-        // Copy current scores to old scores
-        #pragma omp parallel for
+
+    while (!converge) 
+    {
         for (int i = 0; i < numNodes; i++) {
             scoreOld[i] = solution[i];
         }
 
-        // First, compute basic PageRank contribution
-        #pragma omp parallel for
         for (int vi = 0; vi < numNodes; vi++) {
             double incomingScore = 0.0;
-            
-            // Sum up contributions from all incoming edges
+
             for (const Vertex *in = incoming_begin(g, vi); in != incoming_end(g, vi); ++in) {
                 Vertex vj = *in;
                 int totalOut = outgoing_size(g, vj);
-                incomingScore += scoreOld[vj] / totalOut;
+                if (totalOut > 0) {
+                    incomingScore += scoreOld[vj] / totalOut;
+                }
             }
             
-            // Apply damping factor
             solution[vi] = (damping * incomingScore) + ((1.0 - damping) / numNodes);
         }
 
-        // Handle dangling nodes (nodes with no outgoing edges)
-        double danglingSum = 0.0;
-        #pragma omp parallel for reduction(+:danglingSum)
         for (int vi = 0; vi < numNodes; vi++) {
             if (outgoing_size(g, vi) == 0) {
-                danglingSum += scoreOld[vi];
+                solution[vi] += (damping * scoreOld[vi]) / numNodes;
             }
         }
-        
-        // Distribute dangling node contributions
-        double danglingContrib = (damping * danglingSum) / numNodes;
-        #pragma omp parallel for
-        for (int vi = 0; vi < numNodes; vi++) {
-            solution[vi] += danglingContrib;
-        }
 
-        // Check for convergence
         double globDiff = 0.0;
-        #pragma omp parallel for reduction(+:globDiff)
         for (int vi = 0; vi < numNodes; vi++) {
-            globDiff += fabs(solution[vi] - scoreOld[vi]);
+            globDiff += fabs(solution[vi] - scoreOld[vi]);  
         }
         
-        converge = (globDiff < convergence);
+        converge = globDiff < convergence;
     }
 
     free(scoreOld);
+  /*
+     For PP students: Implement the page rank algorithm here.  You
+     are expected to parallelize the algorithm using openMP.  Your
+     solution may need to allocate (and free) temporary arrays.
+
+     Basic page rank pseudocode is provided below to get you started:
+
+     // initialization: see example code above
+     score_old[vi] = 1/numNodes;
+
+     while (!converged) {
+
+       // compute score_new[vi] for all nodes vi:
+       score_new[vi] = sum over all nodes vj reachable from incoming edges
+                          { score_old[vj] / number of edges leaving vj  }
+       score_new[vi] = (damping * score_new[vi]) + (1.0-damping) / numNodes;
+
+       score_new[vi] += sum over all nodes v in graph with no outgoing edges
+                          { damping * score_old[v] / numNodes }
+
+       // compute how much per-node scores have changed
+       // quit once algorithm has converged
+
+       global_diff = sum over all nodes vi { abs(score_new[vi] - score_old[vi]) };
+       converged = (global_diff < convergence)
+     }
+
+   */
 }
