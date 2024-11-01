e1488e68aa1ca74839a48eb2930f41944cc1805a
jjmow <dagantegan.ms09@nctu.edu.tw>
2024-11-01 09:17:20 +0800

    hybrid try2

--- HW3/part2/breadth_first_search/bfs.cpp
+++ HW3/part2/breadth_first_search/bfs.cpp
@@ -105,7 +105,7 @@ void bfs_top_down(Graph graph, solution *sol)
 }
 
 vertex_set* bottom_up_step(Graph g, int *distances, int curDis) {
-    vertex_set list2 = (vertex_set*) malloc(sizeof(vertex_set));
+    vertex_set* list2 = (vertex_set*) malloc(sizeof(vertex_set));
 
     vertex_set_init(&list2, g->num_nodes);
 
