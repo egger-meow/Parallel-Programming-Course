974100ac5a961f67a253df33c453bf3809a44cf7
jjmow <dagantegan.ms09@nctu.edu.tw>
2024-11-01 10:12:17 +0800

    jj

--- HW3/part2/breadth_first_search/bfs.cpp
+++ HW3/part2/breadth_first_search/bfs.cpp
@@ -169,6 +169,7 @@ void bfs_hybrid(Graph graph, solution *sol)
 {
     int numNodes = graph -> num_nodes;
     int threshold  = static_cast <int> (round(sqrt( static_cast <float>(numNodes))));
+    int threshold  = 1000;
 
     vertex_set list1;
     vertex_set list2;
