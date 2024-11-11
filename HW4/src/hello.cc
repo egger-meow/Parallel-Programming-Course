f0ff9f6ec135238e708fadb1174ed4c1ebadb66d
jjmow <dagantegan.ms09@nctu.edu.tw>
2024-11-12 00:09:54 +0800

    warmup2

--- HW4/src/hello.cc
+++ HW4/src/hello.cc
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 
     // TODO: Get the number of processes
     int world_size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     // TODO: Get the rank of the process
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
