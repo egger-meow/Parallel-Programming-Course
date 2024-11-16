4e5c37e1cc9543b3b2538b543bde53bda04cbb38
jjmow <dagantegan.ms09@nctu.edu.tw>
2024-11-16 12:21:13 +0800

    p1-done

--- HW4/src/pi_block_linear.cc
+++ HW4/src/pi_block_linear.cc
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+typedef long long int lli
 int main(int argc, char **argv)
 {
     // --- DON'T TOUCH ---
@@ -16,20 +17,48 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: init MPI
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    lli tossesLocal = tosses / world_size;
+    lli remainder = tossed % world_size;
+    if (world_rank < remainder) 
+        tossesLocal += 1;
+
+    unsigned int seed = (unsigned int) time(NULL) + world_rank * 100;
+
+    lli countLocal = 0;
+
+    for (lli i = 0; i < tossesLocal; i++) {
+        double x = (double) rand_r(&seed) / RAND_MAX;
+        double y = (double) rand_r(&seed) / RAND_MAX;
+        if (x * x + y * y <= 1.0) 
+            countLocal++;
+        
+    }
 
     if (world_rank > 0)
     {
         // TODO: handle workers
+        MPI_Send(&countLocal, 1, MPI_LONG_LONG_INT, 0, 0, MPI_COMM_WORLD);
     }
     else if (world_rank == 0)
     {
         // TODO: master
+        lli counTotal = countLocal;
+        lli count;
+
+        for (int i = 1; i < world_size; i++) {
+            MPI_Recv(&recv_count, 1, MPI_LONG_LONG_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            counTotal += count;
+        }
+        pi_result = 4.0 * (double) counTotal / (double) tosses;
+
     }
 
     if (world_rank == 0)
     {
         // TODO: process PI result
-
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
         printf("%lf\n", pi_result);
