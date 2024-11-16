457694be5531d8c35a387a9b9e347a7b0f2d3399
jjmow <dagantegan.ms09@nctu.edu.tw>
2024-11-16 16:09:01 +0800

    p2-done

--- HW4/src/pi_block_linear.cc
+++ HW4/src/pi_block_linear.cc
@@ -5,7 +5,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-typedef long long int lli;
+typedef long long int lln;
 int main(int argc, char **argv)
 {
     // --- DON'T TOUCH ---
@@ -20,21 +20,20 @@ int main(int argc, char **argv)
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    lli tossesLocal = tosses / world_size;
-    lli remainder = tosses % world_size;
+    lln tossesLocal = tosses / world_size;
+    lln remainder = tosses % world_size;
     if (world_rank < remainder) 
         tossesLocal += 1;
 
     unsigned int seed = (unsigned int) time(NULL) + world_rank * 100;
 
-    lli countLocal = 0;
+    lln countLocal = 0;
 
-    for (lli i = 0; i < tossesLocal; i++) {
+    for (lln i = 0; i < tossesLocal; i++) {
         double x = (double) rand_r(&seed) / RAND_MAX;
         double y = (double) rand_r(&seed) / RAND_MAX;
         if (x * x + y * y <= 1.0) 
             countLocal++;
-        
     }
 
     if (world_rank > 0)
@@ -45,8 +44,8 @@ int main(int argc, char **argv)
     else if (world_rank == 0)
     {
         // TODO: master
-        lli counTotal = countLocal;
-        lli count;
+        lln counTotal = countLocal;
+        lln count;
 
         for (int i = 1; i < world_size; i++) {
             MPI_Recv(&count, 1, MPI_LONG_LONG_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
--- HW4/src/pi_block_tree.cc
+++ HW4/src/pi_block_tree.cc
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+typedef long long int lln;
 int main(int argc, char **argv)
 {
     // --- DON'T TOUCH ---
@@ -16,13 +17,48 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: MPI init
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
     // TODO: binary tree redunction
+
+    lln tossesLocal = tosses / world_size;
+    lln remainder = tosses % world_size;
+    if (world_rank < remainder) 
+        tossesLocal += 1;
+
+    unsigned int seed = (unsigned int) time(NULL) + world_rank * 100;
+
+    lln countLocal = 0;
+
+    for (lln i = 0; i < tossesLocal; i++) {
+        double x = (double) rand_r(&seed) / RAND_MAX;
+        double y = (double) rand_r(&seed) / RAND_MAX;
+        if (x * x + y * y <= 1.0) 
+            countLocal++;
+    }
+
+    int remain = world_size - world_rank;
+    lln counTotal = countLocal;
+    lln count;
+
+    int counter = 1;
+    while (remain > 1) {
+        if (remain % 2 == 1) {
+            MPI_Send(&countLocal, 1, MPI_LONG_LONG_INT, world_rank - counter, 0, MPI_COMM_WORLD);
+            break;
+        } else {
+            MPI_Recv(&count, 1, MPI_LONG_LONG_INT, world_rank + counter, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            counTotal += count;
+        }
+        remain /= 2;
+        counter *= 2;
+    }
 
     if (world_rank == 0)
     {
         // TODO: PI result
-
+        pi_result = 4.0 * (double) counTotal / (double) tosses;
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
         printf("%lf\n", pi_result);
