237fc0f57c2cd8491bddc08439fcac136eaef803
jjmow <dagantegan.ms09@nctu.edu.tw>
2024-11-16 16:32:58 +0800

    p2-done

--- HW4/src/pi_block_tree.cc
+++ HW4/src/pi_block_tree.cc
@@ -44,9 +44,8 @@ int main(int argc, char **argv)
 
     int counter = 1;
     while (remain > 0) {
-        if (remain % 2 == 1) {
+        if (remain % 2 == 1 && world_rank != 0) {
             MPI_Send(&countLocal, 1, MPI_LONG_LONG_INT, world_rank-counter, 0, MPI_COMM_WORLD);
-            printf("%d \n",world_size);
             break;
         } else {
             MPI_Recv(&count, 1, MPI_LONG_LONG_INT, world_rank + counter, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
