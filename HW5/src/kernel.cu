5f0ae517a1928e5050a460de096ae4cbed503984
jjmow <dagantegan.ms09@nctu.edu.tw>
2024-11-24 12:28:01 +0800

    tt

--- HW5/src/kernel.cu
+++ HW5/src/kernel.cu
@@ -1,6 +1,22 @@
 #include <cuda.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define THREADS_PER_BLOCK 256
+
+__device__ int mandel(float c_re, float c_im, int maxIterations) {
+    float z_re = c_re, z_im = c_im;
+    int i;
+    for (i = 0; i < maxIterations; ++i) {
+        if (z_re * z_re + z_im * z_im > 4.f)
+            break;
+
+        float new_re = z_re * z_re - z_im * z_im;
+        float new_im = 2.f * z_re * z_im;
+        z_re = c_re + new_re;
+        z_im = c_im + new_im;
+    }
+    return i;
+}
 
 __global__ void mandelKernel(float lowerX, float lowerY, float stepX, float stepY, 
                              int maxIterations, int* img, int resX, int resY) {
