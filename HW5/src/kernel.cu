1b240b1cae3162895780226de91ce6a66bb2ccdd
jjmow <dagantegan.ms09@nctu.edu.tw>
2024-11-25 00:16:10 +0800

    t33

--- HW5/src/kernel.cu
+++ HW5/src/kernel.cu
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define THREADS_PER_BLOCK 128
+#define GROUP_SIZE 8
 
 __device__ int mandel(float c_re, float c_im, int maxIterations) {
     float z_re = c_re, z_im = c_im;
@@ -18,17 +19,21 @@ __device__ int mandel(float c_re, float c_im, int maxIterations) {
     return i;
 }
 
-__global__ void mandelKernel(float lowerX, float lowerY, float stepX, float stepY, 
-                             int maxIterations, int* img, size_t pitch, int resX, int resY) {
+__global__ void mandelKernel (float lowerX, float lowerY, float stepX, float stepY, 
+                             int maxIterations, int* img, size_t pitch, int resX, int resY)  {
     // To avoid error caused by the floating number, use the following pseudo code
     //
     // float x = lowerX + thisX * stepX;
-    // float y = lowerY + thisY * stepY;'
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    
-    if (idx < resX * resY) {
-        int x = idx % resX;
-        int y = idx / resX;
+    // float y = lowerY + thisY * stepY;
+    int threadID = blockIdx.x * blockDim.x + threadIdx.x;
+    for (int group = 0; group < GROUP_SIZE; ++group) {
+        int idx = threadID * GROUP_SIZE + group;
+
+        if (idx >= resX * resY)
+            continue;
+
+        int x = idx % resX;     
+        int y = idx / resX;      
 
         float c_re = lowerX + x * stepX;
         float c_im = lowerY + y * stepY;
@@ -37,7 +42,7 @@ __global__ void mandelKernel(float lowerX, float lowerY, float stepX, float step
 
         int* row = (int*)((char*)img + y * pitch);
         row[x] = iter;
-    } 
+    }
 }
 
 // Host front-end function that allocates the memory and launches the GPU kernel
@@ -48,6 +53,7 @@ void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, i
 
     int threadsPerBlockLocal = THREADS_PER_BLOCK;
     int blocksPerGrid = (resX * resY + threadsPerBlockLocal - 1) / threadsPerBlockLocal;
+
 
     int* hostImg;
     cudaHostAlloc((void**)&hostImg, resX * resY * sizeof(int), cudaHostAllocDefault);
