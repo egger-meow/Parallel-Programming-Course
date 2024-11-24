7a34825ffdb2266af2e6da510a709952af937262
jjmow <dagantegan.ms09@nctu.edu.tw>
2024-11-25 00:25:50 +0800

    1

--- HW5/src/kernel.cu
+++ HW5/src/kernel.cu
@@ -1,8 +1,7 @@
 #include <cuda.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define THREADS_PER_BLOCK 256
-#define GROUP_SIZE 2
+#define THREADS_PER_BLOCK 128
 
 __device__ int mandel(float c_re, float c_im, int maxIterations) {
     float z_re = c_re, z_im = c_im;
@@ -19,30 +18,25 @@ __device__ int mandel(float c_re, float c_im, int maxIterations) {
     return i;
 }
 
-__global__ void mandelKernel (float lowerX, float lowerY, float stepX, float stepY, 
-                             int maxIterations, int* img, size_t pitch, int resX, int resY)  {
+__global__ void mandelKernel (float lowerX, float lowerY, float stepX, float stepY, int maxIterations, int* img, int resX, int resY) {
     // To avoid error caused by the floating number, use the following pseudo code
     //
     // float x = lowerX + thisX * stepX;
     // float y = lowerY + thisY * stepY;
-    int threadID = blockIdx.x * blockDim.x + threadIdx.x;
-    for (int group = 0; group < GROUP_SIZE; ++group) {
-        int idx = threadID * GROUP_SIZE + group;
 
-        if (idx >= resX * resY)
-            continue;
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-        int x = idx % resX;     
-        int y = idx / resX;      
+    if (idx < resX * resY) {
+        int x = idx % resX;
+        int y = idx / resX;
 
         float c_re = lowerX + x * stepX;
         float c_im = lowerY + y * stepY;
 
         int iter = mandel(c_re, c_im, maxIterations);
 
-        int* row = (int*)((char*)img + y * pitch);
-        row[x] = iter;
-    }
+        img[y * resX + x] = iter;
+    } 
 }
 
 // Host front-end function that allocates the memory and launches the GPU kernel
@@ -51,26 +45,23 @@ void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, i
     float stepX = (upperX - lowerX) / resX;
     float stepY = (upperY - lowerY) / resY;
 
-    int threadsPerBlockLocal = THREADS_PER_BLOCK;
-    int blocksPerGrid = (resX * resY + threadsPerBlockLocal - 1) / threadsPerBlockLocal;
+    size_t size = resX * resY * sizeof(int);
 
-
-    int* hostImg;
-    cudaHostAlloc((void**)&hostImg, resX * resY * sizeof(int), cudaHostAllocDefault);
-
+    int* hostImg = (int*)malloc(size);
     int* devImg;
-    size_t pitch;
-    cudaMallocPitch((void**)&devImg, &pitch, resX * sizeof(int), resY);
+    cudaMalloc((void**)&devImg, size);
+    
+    int threadsPerBlock = THREADS_PER_BLOCK;
+    int blocksPerGrid = (resX * resY + threadsPerBlock - 1) / threadsPerBlock;
 
-    mandelKernel<<<blocksPerGrid, threadsPerBlockLocal>>>(
-        lowerX, lowerY, stepX, stepY, maxIterations, devImg, pitch, resX, resY);
+    mandelKernel<<<blocksPerGrid, threadsPerBlock>>>(
+        lowerX, lowerY, stepX, stepY, maxIterations, devImg, resX, resY);
 
-    cudaDeviceSynchronize();    
-    cudaMemcpy2D(hostImg, resX * sizeof(int), devImg, pitch, resX * sizeof(int), resY, cudaMemcpyDeviceToHost);
+    cudaMemcpy(hostImg, devImg, size, cudaMemcpyDeviceToHost);
 
     for (int i = 0; i < resX * resY; ++i) 
         img[i] = hostImg[i];
-    
+
     cudaFree(devImg);
-    cudaFreeHost(hostImg);
+    free(hostImg);
 }
