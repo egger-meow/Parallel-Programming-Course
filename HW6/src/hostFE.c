d85a073457f936bdf11e4261c516e5cb71eb7e1b
jjmow <dagantegan.ms09@nctu.edu.tw>
2024-12-08 01:25:16 +0800

    clear

--- HW6/src/hostFE.c
+++ HW6/src/hostFE.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "hostFE.h"
 #include "helper.h"
-
-#define MAX_SOURCE_SIZE (0x100000)
 
 void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
             float *inputImage, float *outputImage, cl_device_id *device,
@@ -12,247 +9,85 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
 {
     cl_int status;
     int filterSize = filterWidth * filterWidth;
-
-    // Select platform
-    cl_platform_id platform_id = NULL;
-    cl_uint ret_num_platforms;
-    status = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
-    if (status != CL_SUCCESS) {
-        
-    fprintf(stderr, "OpenCL error %d: Failed to get platform IDs\n", status);
-    return;
-;
-    }
-
-    // Select device (GPU)
-    cl_device_id device_id = NULL;
-    cl_uint ret_num_devices;
-    status = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, &ret_num_devices);
-    if (status != CL_SUCCESS) {
-        
-    fprintf(stderr, "OpenCL error %d: Failed to get platform IDs\n", status);
-    return;
-;
-    }
-
-    // Create context
-    cl_context context_local = clCreateContext(NULL, 1, &device_id, NULL, NULL, &status);
-    if (status != CL_SUCCESS) {
-        
-    fprintf(stderr, "OpenCL error %d: Failed to get platform IDs\n", status);
-    return;
-;
-    }
-
+    
     // Create command queue
-    cl_command_queue command_queue = clCreateCommandQueue(context_local, device_id, 0, &status);
+    cl_command_queue cmdQueue = clCreateCommandQueue(*context, *device, 0, &status);
+    if (status != CL_SUCCESS) return;
+
+    // Create memory buffers
+    cl_mem inputBuffer = clCreateBuffer(*context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+                                      sizeof(float) * imageWidth * imageHeight,
+                                      inputImage, &status);
     if (status != CL_SUCCESS) {
-        clReleaseContext(context_local);
-        
-    fprintf(stderr, "OpenCL error %d: Failed to get platform IDs\n", status);
-    return;
-;
+        clReleaseCommandQueue(cmdQueue);
+        return;
     }
 
-    // Create buffers
-    cl_mem input_mem = clCreateBuffer(context_local, CL_MEM_READ_ONLY,
-                                      sizeof(float)*imageWidth*imageHeight, NULL, &status);
+    cl_mem filterBuffer = clCreateBuffer(*context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+                                       sizeof(float) * filterSize,
+                                       filter, &status);
     if (status != CL_SUCCESS) {
-        clReleaseCommandQueue(command_queue);
-        clReleaseContext(context_local);
-        
-    fprintf(stderr, "OpenCL error %d: Failed to get platform IDs\n", status);
-    return;
-;
+        clReleaseMemObject(inputBuffer);
+        clReleaseCommandQueue(cmdQueue);
+        return;
     }
 
-    cl_mem filter_mem = clCreateBuffer(context_local, CL_MEM_READ_ONLY,
-                                       sizeof(float)*filterSize, NULL, &status);
+    cl_mem outputBuffer = clCreateBuffer(*context, CL_MEM_WRITE_ONLY,
+                                       sizeof(float) * imageWidth * imageHeight,
+                                       NULL, &status);
     if (status != CL_SUCCESS) {
-        clReleaseMemObject(input_mem);
-        clReleaseCommandQueue(command_queue);
-        clReleaseContext(context_local);
-        
-    fprintf(stderr, "OpenCL error %d: Failed to get platform IDs\n", status);
-    return;
-;
+        clReleaseMemObject(filterBuffer);
+        clReleaseMemObject(inputBuffer);
+        clReleaseCommandQueue(cmdQueue);
+        return;
     }
 
-    cl_mem output_mem = clCreateBuffer(context_local, CL_MEM_WRITE_ONLY,
-                                       sizeof(float)*imageWidth*imageHeight, NULL, &status);
+    // Create the kernel
+    cl_kernel kernel = clCreateKernel(*program, "convolution", &status);
     if (status != CL_SUCCESS) {
-        clReleaseMemObject(filter_mem);
-        clReleaseMemObject(input_mem);
-        clReleaseCommandQueue(command_queue);
-        clReleaseContext(context_local);
-        
-    fprintf(stderr, "OpenCL error %d: Failed to get platform IDs\n", status);
-    return;
-;
+        clReleaseMemObject(outputBuffer);
+        clReleaseMemObject(filterBuffer);
+        clReleaseMemObject(inputBuffer);
+        clReleaseCommandQueue(cmdQueue);
+        return;
     }
 
-    // Write data to buffers
-    status = clEnqueueWriteBuffer(command_queue, input_mem, CL_TRUE, 0,
-                                  sizeof(float)*imageWidth*imageHeight, inputImage, 0, NULL, NULL);
-    if (status != CL_SUCCESS) {
-        clReleaseMemObject(output_mem);
-        clReleaseMemObject(filter_mem);
-        clReleaseMemObject(input_mem);
-        clReleaseCommandQueue(command_queue);
-        clReleaseContext(context_local);
-        
-    fprintf(stderr, "OpenCL error %d: Failed to get platform IDs\n", status);
-    return;
-;
-    }
-
-    status = clEnqueueWriteBuffer(command_queue, filter_mem, CL_TRUE, 0,
-                                  sizeof(float)*filterSize, filter, 0, NULL, NULL);
-    if (status != CL_SUCCESS) {
-        clReleaseMemObject(output_mem);
-        clReleaseMemObject(filter_mem);
-        clReleaseMemObject(input_mem);
-        clReleaseCommandQueue(command_queue);
-        clReleaseContext(context_local);
-        
-    fprintf(stderr, "OpenCL error %d: Failed to get platform IDs\n", status);
-    return;
-;
-    }
-
-    // Read kernel source
-    FILE *fp = fopen("kernel.cl", "r");
-    if (!fp) {
-        clReleaseMemObject(output_mem);
-        clReleaseMemObject(filter_mem);
-        clReleaseMemObject(input_mem);
-        clReleaseCommandQueue(command_queue);
-        clReleaseContext(context_local);
-        
-    fprintf(stderr, "OpenCL error %d: Failed to get platform IDs\n", status);
-    return;
-;
-    }
-
-    char *source_str = (char*)malloc(MAX_SOURCE_SIZE);
-    size_t source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
-    fclose(fp);
-
-    // Create and build program
-    cl_program program_local = clCreateProgramWithSource(context_local, 1,
-                                                         (const char **)&source_str,
-                                                         (const size_t *)&source_size,
-                                                         &status);
-    free(source_str);
-    if (status != CL_SUCCESS) {
-        clReleaseMemObject(output_mem);
-        clReleaseMemObject(filter_mem);
-        clReleaseMemObject(input_mem);
-        clReleaseCommandQueue(command_queue);
-        clReleaseContext(context_local);
-        
-    fprintf(stderr, "OpenCL error %d: Failed to get platform IDs\n", status);
-    return;
-;
-    }
-
-    status = clBuildProgram(program_local, 1, &device_id, NULL, NULL, NULL);
-    if (status != CL_SUCCESS) {
-        clReleaseProgram(program_local);
-        clReleaseMemObject(output_mem);
-        clReleaseMemObject(filter_mem);
-        clReleaseMemObject(input_mem);
-        clReleaseCommandQueue(command_queue);
-        clReleaseContext(context_local);
-        
-    fprintf(stderr, "OpenCL error %d: Failed to get platform IDs\n", status);
-    return;
-;
-    }
-
-    // Create kernel
-    cl_kernel kernel = clCreateKernel(program_local, "convolution", &status);
-    if (status != CL_SUCCESS) {
-        clReleaseProgram(program_local);
-        clReleaseMemObject(output_mem);
-        clReleaseMemObject(filter_mem);
-        clReleaseMemObject(input_mem);
-        clReleaseCommandQueue(command_queue);
-        clReleaseContext(context_local);
-        
-    fprintf(stderr, "OpenCL error %d: Failed to get platform IDs\n", status);
-    return;
-;
-    }
-
-    // Set kernel args
-    status  = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&input_mem);
-    status |= clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&filter_mem);
-    status |= clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&output_mem);
+    // Set kernel arguments
+    status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputBuffer);
+    status |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &filterBuffer);
+    status |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &outputBuffer);
     status |= clSetKernelArg(kernel, 3, sizeof(int), &imageWidth);
     status |= clSetKernelArg(kernel, 4, sizeof(int), &imageHeight);
     status |= clSetKernelArg(kernel, 5, sizeof(int), &filterWidth);
 
+    // Set work-item dimensions
+    size_t localWS[2] = {16, 16};  // Work-group size
+    size_t globalWS[2] = {
+        ((imageWidth + localWS[0] - 1) / localWS[0]) * localWS[0],
+        ((imageHeight + localWS[1] - 1) / localWS[1]) * localWS[1]
+    };
+
+    // Execute kernel
+    status = clEnqueueNDRangeKernel(cmdQueue, kernel, 2, NULL,
+                                   globalWS, localWS, 0, NULL, NULL);
     if (status != CL_SUCCESS) {
         clReleaseKernel(kernel);
-        clReleaseProgram(program_local);
-        clReleaseMemObject(output_mem);
-        clReleaseMemObject(filter_mem);
-        clReleaseMemObject(input_mem);
-        clReleaseCommandQueue(command_queue);
-        clReleaseContext(context_local);
-        
-    fprintf(stderr, "OpenCL error %d: Failed to get platform IDs\n", status);
-    return;
-;
+        clReleaseMemObject(outputBuffer);
+        clReleaseMemObject(filterBuffer);
+        clReleaseMemObject(inputBuffer);
+        clReleaseCommandQueue(cmdQueue);
+        return;
     }
 
-    // Run kernel
-    size_t global_size[2] = {(size_t)imageWidth, (size_t)imageHeight};
-    size_t local_size[2] = {16, 16}; // Adjust if necessary
-    status = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL,
-                                    global_size, local_size, 0, NULL, NULL);
-    if (status != CL_SUCCESS) {
-        clReleaseKernel(kernel);
-        clReleaseProgram(program_local);
-        clReleaseMemObject(output_mem);
-        clReleaseMemObject(filter_mem);
-        clReleaseMemObject(input_mem);
-        clReleaseCommandQueue(command_queue);
-        clReleaseContext(context_local);
-        
-    fprintf(stderr, "OpenCL error %d: Failed to get platform IDs\n", status);
-    return;
-;
-    }
-
-    // Finish queue
-    clFinish(command_queue);
-
-    // Read result
-    status = clEnqueueReadBuffer(command_queue, output_mem, CL_TRUE, 0,
-                                 sizeof(float)*imageWidth*imageHeight, outputImage,
-                                 0, NULL, NULL);
-    if (status != CL_SUCCESS) {
-        clReleaseKernel(kernel);
-        clReleaseProgram(program_local);
-        clReleaseMemObject(output_mem);
-        clReleaseMemObject(filter_mem);
-        clReleaseMemObject(input_mem);
-        clReleaseCommandQueue(command_queue);
-        clReleaseContext(context_local);
-        
-    fprintf(stderr, "OpenCL error %d: Failed to get platform IDs\n", status);
-    return;
-;
-    }
+    // Read back results
+    status = clEnqueueReadBuffer(cmdQueue, outputBuffer, CL_TRUE, 0,
+                                sizeof(float) * imageWidth * imageHeight,
+                                outputImage, 0, NULL, NULL);
 
     // Cleanup
     clReleaseKernel(kernel);
-    clReleaseProgram(program_local);
-    clReleaseMemObject(output_mem);
-    clReleaseMemObject(filter_mem);
-    clReleaseMemObject(input_mem);
-    clReleaseCommandQueue(command_queue);
-    clReleaseContext(context_local);
+    clReleaseMemObject(outputBuffer);
+    clReleaseMemObject(filterBuffer);
+    clReleaseMemObject(inputBuffer);
+    clReleaseCommandQueue(cmdQueue);
 }
--- HW6/src/kernel.cl
+++ HW6/src/kernel.cl
@@ -1,59 +1,39 @@
 __kernel void convolution(
-    __global const float* inputImage,
+    __global const float* input,
     __global const float* filter,
-    __global float* outputImage,
-    const int imageWidth,
-    const int imageHeight,
-    const int filterWidth,
-    __local float* localInput
-) {
-    // Get local and global coordinates
-    const int localX = get_local_id(0);
-    const int localY = get_local_id(1);
-    const int globalX = get_global_id(0);
-    const int globalY = get_global_id(1);
-    const int localWidth = get_local_size(0);
-    const int localHeight = get_local_size(1);
+    __global float* output,
+    const int width,
+    const int height,
+    const int filterWidth)
+{
+    // Get global ID
+    const int x = get_global_id(0);
+    const int y = get_global_id(1);
+    
+    // Return if out of bounds
+    if (x >= width || y >= height) return;
     
     const int halfFilter = filterWidth / 2;
-    // Calculate input tile size including padding for filter
-    const int inputTileWidth = localWidth + 2 * halfFilter;
-    const int inputTileHeight = localHeight + 2 * halfFilter;
-    
-    // Load input tile into local memory
-    for (int y = localY; y < inputTileHeight; y += localHeight) {
-        for (int x = localX; x < inputTileWidth; x += localWidth) {
-            int globalInputX = globalX - halfFilter + (x - localX);
-            int globalInputY = globalY - halfFilter + (y - localY);
-            
-            // Handle boundary conditions with zero padding
-            float value = 0.0f;
-            if (globalInputX >= 0 && globalInputX < imageWidth &&
-                globalInputY >= 0 && globalInputY < imageHeight) {
-                value = inputImage[globalInputY * imageWidth + globalInputX];
-            }
-            
-            localInput[y * inputTileWidth + x] = value;
-        }
-    }
-    
-    barrier(CLK_LOCAL_MEM_FENCE);
-    
-    // Ensure we're within image bounds
-    if (globalX >= imageWidth || globalY >= imageHeight) {
-        return;
-    }
-    
     float sum = 0.0f;
-    for (int i = 0; i < filterWidth; i++) {
-        for (int j = 0; j < filterWidth; j++) {
-            int localInputX = localX + j;
-            int localInputY = localY + i;
-            float filterVal = filter[i * filterWidth + j];
-            float inputVal = localInput[localInputY * inputTileWidth + localInputX];
-            sum += filterVal * inputVal;
+
+    // Loop unrolling for common filter sizes (7x7)
+    #pragma unroll 4
+    for (int i = -halfFilter; i <= halfFilter; i++) {
+        #pragma unroll 4
+        for (int j = -halfFilter; j <= halfFilter; j++) {
+            const int currentY = y + i;
+            const int currentX = x + j;
+            
+            // Check boundaries
+            if (currentY >= 0 && currentY < height && 
+                currentX >= 0 && currentX < width) {
+                const int inputIdx = currentY * width + currentX;
+                const int filterIdx = (i + halfFilter) * filterWidth + (j + halfFilter);
+                sum += input[inputIdx] * filter[filterIdx];
+            }
         }
     }
     
-    outputImage[globalY * imageWidth + globalX] = sum;
+    // Write output
+    output[y * width + x] = sum;
 }
