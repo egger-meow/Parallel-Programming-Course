ef6655acf11a5bc7ac5a9bb09d76e0046ee02575
jjmow <dagantegan.ms09@nctu.edu.tw>
2024-12-08 01:11:43 +0800

    o1

--- HW6/src/hostFE.c
+++ HW6/src/hostFE.c
@@ -1,11 +1,9 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "hostFE.h"
 #include "helper.h"
 
-// Define the maximum source size for the kernel
 #define MAX_SOURCE_SIZE (0x100000)
 
 void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
@@ -15,64 +13,56 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
     cl_int status;
     int filterSize = filterWidth * filterWidth;
 
-    // OpenCL variables
+    // Select platform
     cl_platform_id platform_id = NULL;
-    cl_device_id device_id = NULL;
-    cl_uint ret_num_devices;
     cl_uint ret_num_platforms;
-
-    // Get Platform and Device Info
     status = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
     if (status != CL_SUCCESS) {
-        // Exit if failed to get platform IDs
         exit(1);
     }
 
+    // Select device (GPU)
+    cl_device_id device_id = NULL;
+    cl_uint ret_num_devices;
     status = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, &ret_num_devices);
     if (status != CL_SUCCESS) {
-        // Exit if failed to get device IDs
         exit(1);
     }
 
-    // Create OpenCL context
+    // Create context
     cl_context context_local = clCreateContext(NULL, 1, &device_id, NULL, NULL, &status);
     if (status != CL_SUCCESS) {
-        // Exit if failed to create context
         exit(1);
     }
 
-    // Create Command Queue
+    // Create command queue
     cl_command_queue command_queue = clCreateCommandQueue(context_local, device_id, 0, &status);
     if (status != CL_SUCCESS) {
-        // Release context and exit if failed to create command queue
         clReleaseContext(context_local);
         exit(1);
     }
 
-    // Create Memory Buffers
-    cl_mem input_mem = clCreateBuffer(context_local, CL_MEM_READ_ONLY, 
-                                      sizeof(float) * imageHeight * imageWidth, NULL, &status);
+    // Create buffers
+    cl_mem input_mem = clCreateBuffer(context_local, CL_MEM_READ_ONLY,
+                                      sizeof(float)*imageWidth*imageHeight, NULL, &status);
     if (status != CL_SUCCESS) {
-        // Release resources and exit if failed to create input buffer
         clReleaseCommandQueue(command_queue);
         clReleaseContext(context_local);
         exit(1);
     }
 
-    cl_mem filter_mem = clCreateBuffer(context_local, CL_MEM_READ_ONLY, 
-                                       sizeof(float) * filterSize, NULL, &status);
+    cl_mem filter_mem = clCreateBuffer(context_local, CL_MEM_READ_ONLY,
+                                       sizeof(float)*filterSize, NULL, &status);
     if (status != CL_SUCCESS) {
-        // Release resources and exit if failed to create filter buffer
         clReleaseMemObject(input_mem);
         clReleaseCommandQueue(command_queue);
         clReleaseContext(context_local);
         exit(1);
     }
 
-    cl_mem output_mem = clCreateBuffer(context_local, CL_MEM_WRITE_ONLY, 
-                                       sizeof(float) * imageHeight * imageWidth, NULL, &status);
+    cl_mem output_mem = clCreateBuffer(context_local, CL_MEM_WRITE_ONLY,
+                                       sizeof(float)*imageWidth*imageHeight, NULL, &status);
     if (status != CL_SUCCESS) {
-        // Release resources and exit if failed to create output buffer
         clReleaseMemObject(filter_mem);
         clReleaseMemObject(input_mem);
         clReleaseCommandQueue(command_queue);
@@ -80,11 +70,10 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
         exit(1);
     }
 
-    // Copy input data to memory buffers
-    status = clEnqueueWriteBuffer(command_queue, input_mem, CL_TRUE, 0, 
-                                  sizeof(float) * imageHeight * imageWidth, inputImage, 0, NULL, NULL);
+    // Write data to buffers
+    status = clEnqueueWriteBuffer(command_queue, input_mem, CL_TRUE, 0,
+                                  sizeof(float)*imageWidth*imageHeight, inputImage, 0, NULL, NULL);
     if (status != CL_SUCCESS) {
-        // Release resources and exit if failed to write to input buffer
         clReleaseMemObject(output_mem);
         clReleaseMemObject(filter_mem);
         clReleaseMemObject(input_mem);
@@ -93,10 +82,9 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
         exit(1);
     }
 
-    status = clEnqueueWriteBuffer(command_queue, filter_mem, CL_TRUE, 0, 
-                                  sizeof(float) * filterSize, filter, 0, NULL, NULL);
+    status = clEnqueueWriteBuffer(command_queue, filter_mem, CL_TRUE, 0,
+                                  sizeof(float)*filterSize, filter, 0, NULL, NULL);
     if (status != CL_SUCCESS) {
-        // Release resources and exit if failed to write to filter buffer
         clReleaseMemObject(output_mem);
         clReleaseMemObject(filter_mem);
         clReleaseMemObject(input_mem);
@@ -105,15 +93,9 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
         exit(1);
     }
 
-    // Load the kernel source code into the array source_str
-    FILE *fp;
-    char fileName[] = "kernel.cl";
-    char *source_str;
-    size_t source_size;
-
-    fp = fopen(fileName, "r");
+    // Read kernel source
+    FILE *fp = fopen("kernel.cl", "r");
     if (!fp) {
-        // Release resources and exit if failed to open kernel file
         clReleaseMemObject(output_mem);
         clReleaseMemObject(filter_mem);
         clReleaseMemObject(input_mem);
@@ -122,17 +104,17 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
         exit(1);
     }
 
-    source_str = (char*)malloc(MAX_SOURCE_SIZE);
-    source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
+    char *source_str = (char*)malloc(MAX_SOURCE_SIZE);
+    size_t source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
     fclose(fp);
 
-    // Create a program from the kernel source
-    cl_program program_local = clCreateProgramWithSource(context_local, 1, 
-                                                         (const char **)&source_str, 
-                                                         (const size_t *)&source_size, &status);
+    // Create and build program
+    cl_program program_local = clCreateProgramWithSource(context_local, 1,
+                                                         (const char **)&source_str,
+                                                         (const size_t *)&source_size,
+                                                         &status);
     free(source_str);
     if (status != CL_SUCCESS) {
-        // Release resources and exit if failed to create program
         clReleaseMemObject(output_mem);
         clReleaseMemObject(filter_mem);
         clReleaseMemObject(input_mem);
@@ -141,18 +123,8 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
         exit(1);
     }
 
-    // Build the program
     status = clBuildProgram(program_local, 1, &device_id, NULL, NULL, NULL);
     if (status != CL_SUCCESS) {
-        // Get the build log to understand the error
-        size_t log_size;
-        clGetProgramBuildInfo(program_local, device_id, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
-        char *log = (char*) malloc(log_size);
-        clGetProgramBuildInfo(program_local, device_id, CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
-        // Since printing is not allowed, you might want to write the log to a file or handle it accordingly
-        // For debugging purposes, you can temporarily enable printing here
-        // printf("Build Log:\n%s\n", log);
-        free(log);
         clReleaseProgram(program_local);
         clReleaseMemObject(output_mem);
         clReleaseMemObject(filter_mem);
@@ -162,10 +134,9 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
         exit(1);
     }
 
-    // Create the OpenCL kernel
+    // Create kernel
     cl_kernel kernel = clCreateKernel(program_local, "convolution", &status);
     if (status != CL_SUCCESS) {
-        // Release resources and exit if failed to create kernel
         clReleaseProgram(program_local);
         clReleaseMemObject(output_mem);
         clReleaseMemObject(filter_mem);
@@ -175,15 +146,15 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
         exit(1);
     }
 
-    // Set the arguments of the kernel
+    // Set kernel args
     status  = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&input_mem);
     status |= clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&filter_mem);
     status |= clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&output_mem);
-    status |= clSetKernelArg(kernel, 3, sizeof(int), (void *)&imageWidth);
-    status |= clSetKernelArg(kernel, 4, sizeof(int), (void *)&imageHeight);
-    status |= clSetKernelArg(kernel, 5, sizeof(int), (void *)&filterWidth);
+    status |= clSetKernelArg(kernel, 3, sizeof(int), &imageWidth);
+    status |= clSetKernelArg(kernel, 4, sizeof(int), &imageHeight);
+    status |= clSetKernelArg(kernel, 5, sizeof(int), &filterWidth);
+
     if (status != CL_SUCCESS) {
-        // Release resources and exit if failed to set kernel arguments
         clReleaseKernel(kernel);
         clReleaseProgram(program_local);
         clReleaseMemObject(output_mem);
@@ -194,14 +165,12 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
         exit(1);
     }
 
-    // Execute the OpenCL kernel on the list
-    size_t global_item_size[2] = { (size_t)imageWidth, (size_t)imageHeight };
-    size_t local_item_size[2] = { 16, 16 }; // Adjust as needed based on the device
-
-    status = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL, 
-                                    global_item_size, local_item_size, 0, NULL, NULL);
+    // Run kernel
+    size_t global_size[2] = {(size_t)imageWidth, (size_t)imageHeight};
+    size_t local_size[2] = {16, 16}; // Adjust if necessary
+    status = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL,
+                                    global_size, local_size, 0, NULL, NULL);
     if (status != CL_SUCCESS) {
-        // Release resources and exit if failed to enqueue kernel
         clReleaseKernel(kernel);
         clReleaseProgram(program_local);
         clReleaseMemObject(output_mem);
@@ -212,14 +181,14 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
         exit(1);
     }
 
-    // Wait for the command queue to get serviced before reading back results
+    // Finish queue
     clFinish(command_queue);
 
-    // Read the memory buffer outputImage on the device to the local variable outputImage
-    status = clEnqueueReadBuffer(command_queue, output_mem, CL_TRUE, 0, 
-                                 sizeof(float) * imageHeight * imageWidth, outputImage, 0, NULL, NULL);
+    // Read result
+    status = clEnqueueReadBuffer(command_queue, output_mem, CL_TRUE, 0,
+                                 sizeof(float)*imageWidth*imageHeight, outputImage,
+                                 0, NULL, NULL);
     if (status != CL_SUCCESS) {
-        // Release resources and exit if failed to read output buffer
         clReleaseKernel(kernel);
         clReleaseProgram(program_local);
         clReleaseMemObject(output_mem);
@@ -230,7 +199,7 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
         exit(1);
     }
 
-    // Clean up
+    // Cleanup
     clReleaseKernel(kernel);
     clReleaseProgram(program_local);
     clReleaseMemObject(output_mem);
--- HW6/src/kernel.cl
+++ HW6/src/kernel.cl
@@ -1,5 +1,3 @@
-// kernel.cl
-
 __kernel void convolution(
     __global const float* inputImage,
     __global const float* filter,
@@ -8,32 +6,32 @@ __kernel void convolution(
     const int imageHeight,
     const int filterWidth
 ) {
-    // Get the x and y coordinates of the pixel
     int x = get_global_id(0);
     int y = get_global_id(1);
 
-    // Calculate the half size of the filter
-    int halfFilter = filterWidth / 2;
+    // Ensure valid pixel coordinates
+    if (x >= imageWidth || y >= imageHeight) {
+        return;
+    }
 
+    int halfFilter = filterWidth / 2;
     float sum = 0.0f;
 
-    // Iterate over the filter
     for (int i = -halfFilter; i <= halfFilter; i++) {
         for (int j = -halfFilter; j <= halfFilter; j++) {
             int currentX = x + j;
             int currentY = y + i;
 
-            // Check for boundary conditions (zero-padding)
-            if (currentX >= 0 && currentX < imageWidth && 
-                currentY >= 0 && currentY < imageHeight) {
-                float imageVal = inputImage[currentY * imageWidth + currentX];
-                float filterVal = filter[(i + halfFilter) * filterWidth + (j + halfFilter)];
-                sum += imageVal * filterVal;
+            // Zero-padding condition
+            float imageVal = 0.0f;
+            if (currentX >= 0 && currentX < imageWidth && currentY >= 0 && currentY < imageHeight) {
+                imageVal = inputImage[currentY * imageWidth + currentX];
             }
-            // Else, contribute 0 (implicitly by not adding anything)
+
+            float filterVal = filter[(i + halfFilter)*filterWidth + (j + halfFilter)];
+            sum += imageVal * filterVal;
         }
     }
 
-    // Write the result to the output image
     outputImage[y * imageWidth + x] = sum;
 }
